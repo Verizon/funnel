2636f3fa7368d45d159ca81779b3ff57e144df0a
tmperret <timothy.m.perrett@intel.com>
2013-12-03 00:24:46 -0800

    first pass at the command line cli; seems to have bugs?

--- funnel/src/main/scala/main.scala
+++ funnel/src/main/scala/main.scala
@@ -5,21 +5,37 @@ import scopt.{OptionParser,Read}
 import java.net.URL
 import scala.concurrent.duration._
 
-case class Options(urls: Seq[URL], duration: Duration = 5 minutes)
+case class Options(
+  urls: Seq[(URL,String)], 
+  aggregationInterval: Duration = 5 minutes,
+  retryLimit: Int = 3,
+  healthKey: String = "now/health")
 
 trait FunnelCommandLine {
-  implicit val scoptReadUrl: Read[URL] = Read.reads { new URL(_) }
+  // takes strings of the form:
+  // accounts@http://foobar.com
+  private def splitBucketURLPairs(str: String): (URL,String) = 
+    str.split('@') match {
+      case Array(bucket,urlstring) => (new URL(urlstring),bucket)
+      case _ => sys.error(s"Unable to parse the supplied bucket@url pair: $str")
+    }
+
+  implicit val scoptReadUrl: Read[(URL,String)] = Read.reads { splitBucketURLPairs(_) }
   implicit val scoptReadDuration: Read[Duration] = Read.reads { Duration(_) }
 
   // protected val build = new BuildData
   protected val parser = new OptionParser[Options]("funnel"){
     head("funnel", "1.0")
     
-    opt[Duration]('d',"duration").action { (time, opts) =>
-      opts.copy(duration = time)
-    }.text("Duration is a required property")
-    
-    arg[URL]("<url>[,url]...") unbounded() optional() action { (url, opts) =>
+    opt[Duration]('i',"interval").action { (duration, opts) =>
+      opts.copy(aggregationInterval = duration)
+    }
+
+    opt[Int]('r',"retries").action { (limit, opts) => 
+      opts.copy(retryLimit = limit)
+    }
+
+    arg[(URL,String)]("<url>[,url]...") unbounded() optional() action { (url, opts) =>
       opts.copy(urls = opts.urls :+ url) } text("optional unbounded args")
   }
 
@@ -33,12 +49,20 @@ import scalaz.stream.Process
 object Funnel extends FunnelCommandLine {
   def main(args: Array[String]): Unit = 
     run(args){ options =>  
-      val health = Key[Boolean]("now/health", Units.Healthy)
+      println(">>>> "+ options)
+      val health = Key[Boolean](options.healthKey, Units.Healthy)
+      val events = Events.takeEvery(options.aggregationInterval, options.retryLimit)
 
-      mirrorAndAggregate(Events.takeEvery(options.duration, 5))(Process.emitAll(options.urls), health) {
+      mirrorAndAggregate(events)(Process.emitAll(options.urls), health) {
         case "accounts" => Policies.quorum(2)
-        case "decoding" => Policies.majority
+        case "test"     => Policies.majority
         case _          => sys.error("unknown group type")
       }.run
+
+      println
+      println("Press [Enter] to stop the funnel...")
+      println
+
+      readLine()
     }
 } 
