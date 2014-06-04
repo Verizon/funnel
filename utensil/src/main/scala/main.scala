1e14288424c8325574b3e5a945f1c19670059cad
Runar Bjarnason <runarx.bjarnason@intel.com>
2014-06-04 15:16:48 -0700

    Added the option to monitor the local machine in Utensil

--- utensil/src/main/scala/main.scala
+++ utensil/src/main/scala/main.scala
@@ -76,6 +76,12 @@ object Utensil extends CLI {
       val M = Monitoring.default
       val S = MonitoringServer.start(M, options.funnelPort)
 
+      // Determine whether to generate system statistics for the local host
+      cfg.lookup[Int]("localHostMonitorFrequencySeconds").foreach { t =>
+        implicit val duration = t.seconds
+        Sigar.instrument(new Instruments(1 minute, M))
+      }
+
       val R = RiemannClient.tcp(options.riemann.host, options.riemann.port)
       try {
         R.connect() // urgh. Give me stregth!
