ca134a9bc8c4da5b718cd5018c22d3c7b59983a8
Runar Bjarnason <runarx.bjarnason@intel.com>
2014-07-28 09:59:54 -0700

    Added key descriptions

--- core/src/main/scala/Key.scala
+++ core/src/main/scala/Key.scala
@@ -1,9 +1,13 @@
 package intelmedia.ws.funnel
 
-case class Key[+A] private[funnel](name: String, typeOf: Reportable[A], units: Units[A]) {
+case class Key[+A] private[funnel](name: String,
+                                   typeOf: Reportable[A],
+                                   units: Units[A],
+                                   description: String) {
   def endsWith(suffix: String): Boolean = name.endsWith(suffix)
   def startsWith(prefix: String): Boolean = name.startsWith(prefix)
   def rename(s: String) = copy(name = s)
+  def withDescription(s: String) = copy(description = s)
   def modifyName(f: String => String): Key[A] = rename(f(name))
   def cast[B](R: Reportable[B], U: Units[B]): Option[Key[B]] =
     if (R == typeOf && units == U) Some(this.asInstanceOf[Key[B]])
@@ -22,8 +26,8 @@ object Key {
     override def toString = "Key.EndsWith("+suffix+")"
   }
 
-  def apply[A](name: String, units: Units[A])(implicit R: Reportable[A]): Key[A] =
-    Key(name, R, units)
+  def apply[A](name: String, units: Units[A], desc: String = "")(
+    implicit R: Reportable[A]): Key[A] = Key(name, R, units, desc)
 
   implicit def keyToMetric[A](k: Key[A]): Metric[A] = Metric.key(k)
 }
