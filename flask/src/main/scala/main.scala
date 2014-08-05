498105d7f33985106286cb99b38067c015a29802
tmperret <timothy.m.perrett@intel.com>
2014-08-05 14:12:05 -0700

    removed the CLI trait

--- flask/src/main/scala/main.scala
+++ flask/src/main/scala/main.scala
@@ -17,12 +17,7 @@ import knobs.{Config, Required, ClassPathResource, FileResource}
   *
   * Or pass the location of the config file as a command line argument.
   */
-object Main extends CLI {
-  private def shutdown(server: MonitoringServer, R: RiemannClient): Unit = {
-    server.stop()
-    R.disconnect
-  }
-
+object Main {
   import com.amazonaws.auth.{AWSCredentialsProvider, AWSCredentials}
   import com.amazonaws.services.sns.AmazonSNSClient
   import com.amazonaws.auth.BasicAWSCredentials
@@ -31,6 +26,20 @@ object Main extends CLI {
   import Riemann.Names
   import scalaz.\/._
   import oncue.svc.funnel.aws.SNS
+
+  case class RiemannHostPort(host: String, port: Int)
+
+  case class Options(
+    riemann: RiemannHostPort = RiemannHostPort("localhost", 5555),
+    riemannTTL: Duration = 5 minutes,
+    funnelPort: Int = 5775,
+    transport: DatapointParser = SSE.readEvents _
+  )
+
+  private def shutdown(server: MonitoringServer, R: RiemannClient): Unit = {
+    server.stop()
+    R.disconnect
+  }
 
   private def giveUp(names: Names, cfg: Config, sns: AmazonSNSClient, log: String => Unit) = {
     val msg = s"${names.mine} gave up on ${names.kind} server ${names.theirs}"
@@ -48,97 +57,79 @@ object Main extends CLI {
   }
 
   def main(args: Array[String]): Unit = {
-    run(args){ (options, cfg) =>
-
-      import scalaz.concurrent._
-      val logger = LoggerFactory.getLogger("flask")
-      implicit val logPool = Strategy.Executor(java.util.concurrent.Executors.newFixedThreadPool(1))
-      val L = Actor.actor((s: String) => logger.info(s))
-
-      implicit val log: String => Unit = s => L(s)
-
-      val Q = SNS.client(
-        new BasicAWSCredentials(
-          cfg.require[String]("aws.access-key"),
-          cfg.require[String]("aws.secret-key")),
-        cfg.lookup[String]("aws.proxy-host"),
-        cfg.lookup[Int]("aws.proxy-port"),
-        cfg.lookup[String]("aws.proxy-protocol"),
-        Region.getRegion(Regions.fromName(cfg.require[String]("aws.region")))
-      )
-
-      val M = Monitoring.default
-      val S = MonitoringServer.start(M, options.funnelPort)
-
-      // Determine whether to generate system statistics for the local host
-      for {
-        b <- cfg.lookup[Boolean]("flask.collect-local-metrics") if b == true
-        t <- cfg.lookup[Int]("flask.local-metric-frequency")
-      }{
-        implicit val duration = t.seconds
-        Sigar(new Instruments(1.minute, M)).foreach { s =>
-          s.instrument
-          // Flask.instrument(M, s)
-        }
-      }
-
-      val R = RiemannClient.tcp(options.riemann.host, options.riemann.port)
-      try {
-        R.connect() // urgh. Give me stregth!
-      } catch {
-        case e: java.io.IOException => {
-          errorAndQuit(options,() => shutdown(S,R))
-        }
-      }
-
-      def utensilRetries(names: Names): Event =
-        Riemann.defaultRetries andThen (_ ++ giveUp(names, cfg, Q, log))
-
-      val localhost = java.net.InetAddress.getLocalHost.toString
-
-      Riemann.mirrorAndPublish(
-        M, options.riemannTTL.toSeconds.toFloat, utensilRetries)(
-        R, s"${options.riemann.host}:${options.riemann.port}", utensilRetries)(SSE.readEvents)(
-        S.commands, cfg.lookup[String]("funnelName").getOrElse(localhost))(log
-          ).runAsync(_.fold(e => {
-            e.printStackTrace()
-            log(s"[ERROR] $e - ${e.getMessage}")
-            log(e.getStackTrace.toString)
-          }, identity _))
-    }
-  }
-
-}
-
-import java.net.URL
-import scala.concurrent.duration._
-import java.io.File
-
-trait CLI {
-
-  case class RiemannHostPort(host: String, port: Int)
-
-  case class Options(
-    riemann: RiemannHostPort = RiemannHostPort("localhost", 5555),
-    riemannTTL: Duration = 5 minutes,
-    funnelPort: Int = 5775,
-    transport: DatapointParser = SSE.readEvents _
-  )
-
-  def run(args: Array[String])(f: (Options, Config) => Unit): Unit = {
+    import scalaz.concurrent._
 
     val config =
       knobs.loadImmutable(List(Required(FileResource(new File("/usr/share/oncue/etc/flask.cfg"))))) or
       knobs.loadImmutable(List(Required(ClassPathResource("oncue/flask.cfg"))))
 
-    config.flatMap { cfg =>
+    val cfg = config.run
+
+    val options = config.flatMap { cfg =>
       val port        = cfg.lookup[Int]("flask.network.port").getOrElse(5775)
       val name        = cfg.lookup[String]("flask.name").getOrElse("flask")
       val riemannHost = cfg.lookup[String]("flask.riemann.host").getOrElse("localhost")
       val riemannPort = cfg.lookup[Int]("flask.riemann.port").getOrElse(5555)
       val ttl         = cfg.lookup[Int]("flask.riemann.ttl-in-minutes").getOrElse(5).minutes
-      Task(f(Options(RiemannHostPort(riemannHost, riemannPort), ttl, port), cfg))
+      Task.now(Options(RiemannHostPort(riemannHost, riemannPort), ttl, port))
     }.run
-  }
-}
 
+    val logger = LoggerFactory.getLogger("flask")
+
+    implicit val logPool = Strategy.Executor(java.util.concurrent.Executors.newFixedThreadPool(1))
+
+    val L = Actor.actor((s: String) => logger.info(s))
+
+    implicit val log: String => Unit = s => L(s)
+
+    val Q = SNS.client(
+      new BasicAWSCredentials(
+        cfg.require[String]("aws.access-key"),
+        cfg.require[String]("aws.secret-key")),
+      cfg.lookup[String]("aws.proxy-host"),
+      cfg.lookup[Int]("aws.proxy-port"),
+      cfg.lookup[String]("aws.proxy-protocol"),
+      Region.getRegion(Regions.fromName(cfg.require[String]("aws.region")))
+    )
+
+    val M = Monitoring.default
+    val S = MonitoringServer.start(M, options.funnelPort)
+
+    // Determine whether to generate system statistics for the local host
+    for {
+      b <- cfg.lookup[Boolean]("flask.collect-local-metrics") if b == true
+      t <- cfg.lookup[Int]("flask.local-metric-frequency")
+    }{
+      implicit val duration = t.seconds
+      Sigar(new Instruments(1.minute, M)).foreach { s =>
+        s.instrument
+        // Flask.instrument(M, s)
+      }
+    }
+
+    val R = RiemannClient.tcp(options.riemann.host, options.riemann.port)
+    try {
+      R.connect() // urgh. Give me stregth!
+    } catch {
+      case e: java.io.IOException => {
+        errorAndQuit(options,() => shutdown(S,R))
+      }
+    }
+
+    def utensilRetries(names: Names): Event =
+      Riemann.defaultRetries andThen (_ ++ giveUp(names, cfg, Q, log))
+
+    val localhost = java.net.InetAddress.getLocalHost.toString
+
+    Riemann.mirrorAndPublish(
+      M, options.riemannTTL.toSeconds.toFloat, utensilRetries)(
+      R, s"${options.riemann.host}:${options.riemann.port}", utensilRetries)(SSE.readEvents)(
+      S.commands, cfg.lookup[String]("flask.name").getOrElse(localhost))(log
+        ).runAsync(_.fold(e => {
+          e.printStackTrace()
+          log(s"[ERROR] $e - ${e.getMessage}")
+          log(e.getStackTrace.toString)
+        }, identity _))
+  }
+
+}
