7c1e1b837e0de1a22de580a5a544cfad1ecf1082
tmperret <timothy.m.perrett@intel.com>
2014-08-13 09:38:26 -0700

    updated ASG and EC2

--- aws/src/main/scala/ASG.scala
+++ aws/src/main/scala/ASG.scala
@@ -2,11 +2,24 @@ package oncue.svc.funnel.aws
 
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.auth.{AWSCredentials,BasicAWSCredentials}
+import com.amazonaws.services.ec2.AmazonEC2
+import com.amazonaws.services.ec2.model.Address
 import com.amazonaws.services.autoscaling.{AmazonAutoScaling,AmazonAutoScalingClient}
-import com.amazonaws.services.autoscaling.model.AutoScalingGroup
+import com.amazonaws.services.autoscaling.model.{AutoScalingGroup,Instance => ASGInstance}
 import scalaz.concurrent.Task
+import scala.collection.JavaConverters._
 
 object ASG {
+
+  case class Group(
+    name: String,
+    instances: Seq[Instance] = Nil,
+    tags: Map[String,String] = Map.empty)
+
+  case class Instance(
+    id: String,
+    internalHostname: Option[String] = None,
+    externalHostname: Option[String] = None)
 
   def client(
     credentials: BasicAWSCredentials,
@@ -22,7 +35,37 @@ object ASG {
     client
   }
 
-  def list(client: AmazonAutoScaling): Task[Seq[AutoScalingGroup]] =
-    Task(client.describeAutoScalingGroups.getAutoScalingGroups)
+  def list(asg: AmazonAutoScaling, ec2: AmazonEC2): Task[Seq[Group]] = {
+    def addresses(g: List[Group]): Task[Seq[Address]] = {
+      val x: Seq[Task[Seq[Address]]] = g.flatMap(_.instances.map(i => EC2.instanceHostnames(i.id)(ec2)))
+      Task.gatherUnordered(x).map(_.flatMap(identity))
+    }
+
+    def groups = Task {
+      asg.describeAutoScalingGroups.getAutoScalingGroups.asScala.toList.map(g =>
+        Group(name = g.getAutoScalingGroupName,
+              instances = instances(g),
+              tags = tags(g))
+      )
+    }
+
+    for {
+      g <- groups
+      a <- addresses(g)
+    } yield {
+      println(">>>> " + a)
+
+      g
+    }
+  }
+
+  private def tags(g: AutoScalingGroup): Map[String,String] =
+    g.getTags.asScala.map(t => t.getKey -> t.getValue).toMap
+
+  private def instances(g: AutoScalingGroup): Seq[Instance] =
+    g.getInstances.asScala.map(i => Instance(i.getInstanceId))
+
+  // private def hostnames(i: ASGInstance) = EC2.instance(i.getInstanceId)
+
 
 }
--- aws/src/main/scala/EC2.scala
+++ aws/src/main/scala/EC2.scala
@@ -3,10 +3,17 @@ package oncue.svc.funnel.aws
 import com.amazonaws.regions.{Region, Regions}
 import com.amazonaws.auth.{AWSCredentials,BasicAWSCredentials}
 import com.amazonaws.services.ec2.{AmazonEC2,AmazonEC2Client}
-// import com.amazonaws.services.ec2.model.AutoScalingGroup
+import com.amazonaws.services.ec2.model.{
+  DescribeAddressesRequest,
+  DescribeAddressesResult,
+  Filter,
+  Address}
 import scalaz.concurrent.Task
+import scala.collection.JavaConverters._
 
 object EC2 {
+
+  case class Hostnames(external: Option[String], internal: Option[String])
 
   def client(
     credentials: BasicAWSCredentials,
@@ -14,15 +21,19 @@ object EC2 {
     awsProxyPort: Option[Int] = None,
     awsProxyProtocol: Option[String] = None,
     region: Region = Region.getRegion(Regions.fromName("us-east-1"))
-  ): AmazonAutoScaling = { //cfg.require[String]("aws.region"))
-    val client = new AmazonAutoScalingClient(
+  ): AmazonEC2 = { //cfg.require[String]("aws.region"))
+    val client = new AmazonEC2Client(
       credentials,
       proxy.configuration(awsProxyHost, awsProxyPort, awsProxyProtocol))
     client.setRegion(region)
     client
   }
 
-  def instance()
+  def instanceHostnames(id: String)(ec2: AmazonEC2): Task[Seq[Address]] = Task {
+    ec2.describeAddresses(new DescribeAddressesRequest()).getAddresses.asScala
+  }
+
+  private def filter(n: String) = new Filter().withName(n)
 
   // def list(client: AmazonAutoScaling): Task[Seq[AutoScalingGroup]] =
   //   Task(client.describeAutoScalingGroups.getAutoScalingGroups)
