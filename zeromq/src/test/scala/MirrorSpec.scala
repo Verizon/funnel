614072b0631beaf146560db0c3b51ef116756821
tmperret <timothy.m.perrett@oncue.com>
2015-02-03 10:04:48 -0800

    fixed the mirroring spec for 0mq

--- zeromq/src/test/scala/MirrorSpec.scala
+++ zeromq/src/test/scala/MirrorSpec.scala
@@ -15,15 +15,15 @@ class MirrorSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   lazy val S  = signalOf[Boolean](true)
   lazy val W  = 30.seconds
 
-  lazy val U1 = new URI("tcp://127.0.0.1:7894")
+  lazy val U1 = new URI("ipc:///tmp/u1.socket")
   lazy val E1 = Endpoint.unsafeApply(publish &&& bind, U1)
   lazy val M1 = Monitoring.instance
   lazy val I1 = new Instruments(W, M1)
 
-  lazy val U2 = new URI("tcp://127.0.0.1:7895")
+  lazy val U2 = new URI("ipc:///tmp/u2.socket")
   lazy val E2 = Endpoint.unsafeApply(publish &&& bind, U2)
   lazy val M2 = Monitoring.instance
-  lazy val I2 = new Instruments(W, M1)
+  lazy val I2 = new Instruments(W, M2)
 
   // mirror to this instance
   lazy val MI = Monitoring.instance
@@ -36,13 +36,11 @@ class MirrorSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   private def countKeys(m: Monitoring): Int =
     m.keys.compareAndSet(identity).run.get.filter(_.startsWith("previous")).length
 
-  private def mirrorFrom(uri: URI): Unit ={
-    println(s">>> mirroring from $uri")
+  private def mirrorFrom(uri: URI): Unit =
     MI.mirrorAll(Mirror.from(S)
       )(uri, Map("uri" -> uri.toString)
       ).run.runAsync(_.fold(e => Ø.log.error(
         s"Error mirroring $uri: ${e.getMessage}"), identity))
-  }
 
   override def beforeAll(){
     addInstruments(I1)
@@ -61,17 +59,11 @@ class MirrorSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
   }
 
   override def afterAll(){
-    println("==================================")
-    MI.keys.get.run.foreach(println)
-    println("==================================")
-
-    println("Stoping the signal...")
     stop(S).run
   }
 
   "zeromq mirroring" should "pull values from the specified monitoring instance" in {
-    (countKeys(M1) + countKeys(M2)) should equal (
-      MI.keys.compareAndSet(identity).run.get.length)
+    (countKeys(M1) + countKeys(M2)) should equal (MI.keys.get.run.length)
   }
 
 }
