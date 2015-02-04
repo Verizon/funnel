93878316bd7ea1dedb36ac5cde97fbb44f5af132
tmperret <timothy.m.perrett@oncue.com>
2015-02-04 12:28:33 -0800

    forgot to add the options file

---
+++ flask/src/main/scala/Options.scala
@@ -0,0 +1,16 @@
+package oncue.svc.laboratory
+
+import oncue.svc.funnel.elastic.ElasticCfg
+import scala.concurrent.duration._
+
+case class RiemannCfg(
+  host: String,
+  port: Int,
+  ttl: Duration
+)
+
+case class Options(
+  elastic: Option[ElasticCfg] = None,
+  riemann: Option[RiemannCfg] = None,
+  funnelPort: Int = 5775
+)
