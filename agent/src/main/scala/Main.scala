d6e5c0aaec049087e4b9520ceb35bd8ccd4c331d
tmperret <timothy.m.perrett@oncue.com>
2015-02-05 15:23:51 -0800

    configure the agent with knobs

--- agent/src/main/scala/Main.scala
+++ agent/src/main/scala/Main.scala
@@ -1,15 +1,51 @@
 package oncue.svc.funnel
 package agent
 
+import knobs._
+import scalaz.\/
+import java.io.File
 import journal.Logger
 import zeromq._, sockets._
+import scalaz.concurrent.Task
 import java.net.{InetAddress,URI}
-import scalaz.\/
 
 object Main {
   private val log = Logger[Main.type]
 
+  case class Options(
+    httpHost: String,
+    httpPort: Int,
+    proxySocket: String,
+    proxyHost: String,
+    proxyPort: Int
+  )
+
   def main(args: Array[String]): Unit = {
+    val config: Task[Config] = for {
+      a <- knobs.loadImmutable(List(Required(
+        FileResource(new File("/usr/share/oncue/etc/agent.cfg")) or
+        ClassPathResource("oncue/agent.cfg"))))
+      b <- knobs.aws.config
+    } yield a ++ b
+
+    /**
+     * Create a typed set of options using knobs.
+     * For the proxy host, try looking up the local address of this
+     * particular node and fallback to the config, and then all
+     * known interfaces host if that fails.
+     */
+    val options = config.map { cfg =>
+      Options(
+        cfg.require[String]("agent.http.host"),
+        cfg.lookup[Int]("agent.http.port").getOrElse(8080),
+        cfg.lookup[String]("agent.proxy.socket").getOrElse("/var/run/funnel.socket"),
+        cfg.lookup[String]("aws.network.local-ipv4")
+          .orElse(cfg.lookup[String]("agent.proxy.host"))
+          .getOrElse("0.0.0.0"),
+        cfg.lookup[Int]("agent.proxy.port").getOrElse(8080)
+      )
+    }.run
+
     val (a,i,o) =
       (for {
         x <- \/.fromTryCatchThrowable[String,Exception](InetAddress.getLocalHost.getHostAddress)
@@ -24,7 +60,7 @@ object Main {
     ))
 
     // start the remote instruments server
-    unfiltered.netty.Server.http(8080)
+    unfiltered.netty.Server.http(options.httpPort, options.httpHost)
       .handler(HttpInstruments)
       .run
   }
