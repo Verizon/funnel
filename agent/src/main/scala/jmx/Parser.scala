a6d69856cdb783e43141d194dc12955feed8371b
tmperret <timothy.m.perrett@oncue.com>
2015-02-16 11:22:56 -0800

    fixing the build and documenting the problems with the object name attribute addition

--- agent/src/main/scala/jmx/Parser.scala
+++ agent/src/main/scala/jmx/Parser.scala
@@ -15,6 +15,14 @@ object Parser {
     keyspaces(tail).map(_._2).mkString("/")).toLowerCase
   }
 
+  /**
+   * This really is not ideal as the elements come in from the order they
+   * were actually added to the object name by the creating JMX endpoint,
+   * which unhelpfully varies product to product. As such, this will look
+   * right sometimes, and be backwards in other cases.
+   *
+   * //sigh
+   */
   private[this] def keyspaces(tail: String): Seq[(String,String)] =
     tail.split(',').reverse.map { part =>
       val Array(key,value) = part.trim.split('=')
