a905556d67d6fb64bfd6ff02d8b5217afbea0bd7
tmperret <timothy.m.perrett@oncue.com>
2015-02-27 10:13:54 -0800

    wired the config intot he main runner

--- chemist/src/main/scala/main.scala
+++ chemist/src/main/scala/main.scala
@@ -2,23 +2,19 @@ package funnel
 package chemist
 
 import knobs._
+import java.io.File
 
 object Main {
   def main(args: Array[String]): Unit = {
-    // (for {
-    //   a <- knobs.loadImmutable(Required(
-    //     FileResource(new File("/usr/share/oncue/etc/chemist.cfg")) or
-    //     ClassPathResource("oncue/chemist.cfg")) :: Nil)
-    //   b <- knobs.aws.config
-    //   _ <- exe.run(Config.readConfig(a ++ b))
-    // } yield ()).run
+    val config = (for {
+      a <- knobs.loadImmutable(Required(
+        FileResource(new File("/usr/share/oncue/etc/chemist.cfg")) or
+        ClassPathResource("oncue/chemist.cfg")) :: Nil)
+      b <- knobs.aws.config
+    } yield Config.readConfig(a ++ b)).run
 
-    // alias the `Server` API and what constitutes a chemst server
-    // val S = Server
-    // provide an interpreter for that server
-    // val I = Server0
+    Server.start(config)
 
-    // new Chemist(I, 9000).start()
     // this should probally be called to release
     // the underlying resources.
     // dispatch.Http.shutdown()
