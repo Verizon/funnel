ca70061500960410de480c403fd3fbbcc59fd81d
tmperret <timothy.m.perrett@oncue.com>
2015-03-30 09:36:08 -0700

    removed the double application/json header in the response

--- chemist/src/main/scala/Server.scala
+++ chemist/src/main/scala/Server.scala
@@ -17,9 +17,7 @@ object JsonRequest {
 
 object JsonResponse {
   def apply[A: EncodeJson](a: A, params: PrettyParams = PrettyParams.nospace) =
-    JsonContent ~>
-      ResponseHeader("Content-Type", Set("application/json")) ~>
-      ResponseString(a.jencode.pretty(params))
+    JsonContent ~> ResponseString(a.jencode.pretty(params))
 }
 
 object Server {
