b9021835c6d37f77a81e2b7dec2ea6fd45b2f460
Paul Snively <paul.f.snively@one.verizon.com>
2015-04-06 10:00:41 -0700

    Separated ScalaTest from ScalaCheck.

--- elastic/src/test/scala/ElasticTest.scala
+++ elastic/src/test/scala/ElasticTest.scala
@@ -3,36 +3,7 @@ package elastic
 
 import scala.concurrent.duration._
 import org.scalacheck.{Properties => P, _}
-import org.scalatest._
 import scalaz.stream._
-import scalaz.concurrent.Strategy.Executor
-import scalaz.concurrent.Task
-
-class ElasticSpec extends FlatSpec with Matchers {
-  val scheduler = Monitoring.schedulingPool
-  val S = Executor(Monitoring.serverPool)
-
-  val E = Elastic(Monitoring.default)
-
-  import E._
-
-  "elasticGroup" should "emit on timeout" in {
-    val cfg = ElasticCfg("localhost", "index", "type", "dateFormat", List("k"), 5.seconds, 5000)
-    val dp1 = Datapoint[Any](Key("k1", Units.Count: Units[Double], "description", Map(AttributeKeys.source -> "h1")), 3.14)
-    val dp2 = Datapoint[Any](Key("k2", Units.Count: Units[Double], "description", Map(AttributeKeys.source -> "h2")), 2.17)
-    val dps: Process[Task, Option[Datapoint[Any]]] = (Process(dp1) ++ Process(dp2)).map(Option.apply)
-    val timeout = Process.sleep(5.seconds)(S, scheduler) ++
-      Process(Option.empty[Datapoint[Any]]) ++
-      Process.sleep(5.seconds)(S, scheduler) ++
-      Process(Option.empty[Datapoint[Any]])
-    val input = timeout.wye(dps ++ Process.sleep(15.seconds)(S, scheduler))(wye.merge).translate(lift)
-    val ogs = input |> elasticGroup(List("k"))
-    val result = ogs.runLast.run(cfg).run
-    result should be ('defined)
-    val gs = result.get
-    gs.size shouldBe >= (dps.runLog.run.size)
-  }
-}
 
 object ElasticTest extends P("elastic") {
   val genName = Gen.oneOf("k1", "k2")
