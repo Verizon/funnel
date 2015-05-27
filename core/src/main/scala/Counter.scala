96cd58ea44bef3e597393b37d5a2b08f9114c048
Runar Bjarnason <runar.bjarnason@oncue.com>
2015-05-27 15:18:35 -0400

    Fixed binary compatibility of Counter

--- core/src/main/scala/Counter.scala
+++ core/src/main/scala/Counter.scala
@@ -3,5 +3,11 @@ package oncue.svc.funnel
 class Counter(underlying: PeriodicGauge[Double]) extends Instrument[Periodic[Double]] { self =>
   def incrementBy(by: Int): Unit = underlying append by
   def increment: Unit = incrementBy(1)
+
+  @deprecated("Counters should be monotonically increasing. Use a numericGauge instead.", "2.2")
+  def decrementBy(by: Int): Unit = incrementBy(-by)
+
+  @deprecated("Counters should be monotonically increasing. Use a numericGauge instead.", "2.2")
+  def decrement: Unit = decrementBy(1)
   def keys = underlying.keys
 }
