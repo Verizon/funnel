b1940c8ca803c5d73df3e51a80db1f1d776d3999
tmperret <timothy.m.perrett@oncue.com>
2015-06-11 11:29:02 -0700

    updated the AutoScalingEvent definition to remove AWS specifics from the core chemist module

--- chemist/src/main/scala/Exceptions.scala
+++ chemist/src/main/scala/Exceptions.scala
@@ -9,3 +9,7 @@ case class InstanceNotFoundException(
 case class InvalidLocationException(location: Location) extends RuntimeException {
   override val getMessage: String = s"No hostname is specified for the specified location: $location"
 }
+
+case class NotAFlaskException(e: AutoScalingEvent) extends RuntimeException {
+  override val getMessage: String = s"The specified scaling event was not a flask $e"
+}
