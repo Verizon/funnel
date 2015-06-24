ebde34ee13331955a51004ffa0b84f6496e1fdf2
tmperret <timothy.m.perrett@oncue.com>
2015-06-24 15:50:01 -0700

    added the Ignored LocationIntent; much scaladoc. very wow.

--- chemist/src/main/scala/LocationIntent.scala
+++ chemist/src/main/scala/LocationIntent.scala
@@ -10,11 +10,42 @@ package chemist
 sealed trait LocationIntent
 
 object LocationIntent {
+
+  /**
+   * Make a best effort to convert a string into a `LocationIntent`.
+   * All strings are lowercased and trimmed:
+   * `mirroring`, `supervision`, `ignored`
+   */
   def fromString(s: String): Option[LocationIntent] =
     all.find(_.toString.toLowerCase == s.toLowerCase)
 
   lazy val all = List(Mirroring, Supervision)
 
+  /**
+   * locations destined for mirroring, should be
+   * accessible for Flask to come and collect metrics on.
+   * The protocol should be determined by the templates.
+   */
   case object Mirroring extends LocationIntent
+
+  /**
+   * location destined for supervision are only for use
+   * between flask and chemist components. Supervision
+   * locations provide a channel for the administrative
+   * state and control messages chemist needs to operate.
+   */
   case object Supervision extends LocationIntent
+
+  /**
+   * locations that are meant to be ignored are few and far
+   * between, but this is supplied for completeness. One
+   * case where `Ignored` is useful is when running multiple
+   * clusters of flasks during a migration from one cluster
+   * to another. The "old" flask cluster should be ignored by
+   * the new flasks, and thus have their locations ignored
+   * by chemist. If the old flasks are mirrored by the new
+   * flasks (and vice versa), there would essentially be a
+   * cascading amplication DOS attack of the system, by itself.
+   */
+  case object Ignored extends LocationIntent
 }
