ffa99ec6479488ad11bf8ffb9859a40e1131e511
tmperret <timothy.m.perrett@oncue.com>
2015-06-28 17:44:36 -0700

    Add some debugging to see what the default knobs output is on aws

--- chemist-aws/src/main/scala/Main.scala
+++ chemist-aws/src/main/scala/Main.scala
@@ -5,15 +5,19 @@ package aws
 import scalaz.concurrent.Task
 import scalaz.syntax.monad._
 import http.MonitoringServer
+import journal.Logger
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val log = Logger[Main.type]
+
     val chemist = new AwsChemist[DefaultAws]
 
     val aws = new DefaultAws {
       val config = (for {
         a <- defaultKnobs
         b <- knobs.aws.config
+        _  = log.debug(s"Auto-populated AWS knobs are: $b")
       } yield AwsConfig.readConfig(a ++ b)).run
     }
 
