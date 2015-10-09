788a47f51551c28be00609d01bf80a8b6ba0818b
Runar Bjarnason <runar.bjarnason@oncue.com>
2015-10-09 18:28:37 -0400

    More convenient combinators

--- core/src/main/scala/Metric.scala
+++ core/src/main/scala/Metric.scala
@@ -7,25 +7,41 @@ import scalaz.syntax.applicative._
 
 object Metric {
   /**
-   *
+   * Combines two periodic instruments with keys `a` and `b` into one metric
+   * using the function `f`.
    */
   def combinePeriodic[A:Reportable,B:Reportable,C:Reportable:Group](
     a: Periodic[A],
     b: Periodic[B],
     label: String,
     units: Units,
-    description: String,
+    description: String = "",
     M: Monitoring = Monitoring.default,
-    keyMod: Key[C] => Key[C] = identity[Key[C]] _)(f: (A, B) => C) = {
+    keyMod: Key[C] => Key[C] = identity[Key[C]] _)(f: (A, B) => C): Periodic[C] = {
       val C = implicitly[Group[C]]
       val (ks, _) =
         instruments.periodicBuffers(Buffers.sum(GroupMonoid(C)), (c: C) => c, label, units, description, keyMod)
       val metrics = a.toThree.zipWith(b.toThree) {
         case (k1, k2) => for { x <- liftFC(k1); y <- liftFC(k2) } yield f(x, y)
       }
-      ks.toThree.zipWith(metrics) {
-        case (k, m) => M.publish(k)(Events.changed(k))(m)
-      }
+      Periodic(ks.toThree.zip(metrics).map {
+        case (k, m) => M.publish(k)(Events.changed(k))(m).run
+      })
+  }
+
+  /**
+   * Combines two periodic instruments with keys `a` and `b` into one metric
+   * using the function `f`.
+   */
+  def combineInstruments[A:Reportable,B:Reportable,C:Reportable:Group](
+    a: Instrument[Periodic[A]],
+    b: Instrument[Periodic[B]],
+    label: String,
+    units: Units,
+    description: String = "",
+    M: Monitoring = Monitoring.default,
+    keyMod: Key[C] => Key[C])(f: (A, B) => C) {
+      combinePeriodic(a.keys, b.keys, label, units, description, M, keyMod)(f)
   }
 
   /** Infix syntax for `Metric`. */
