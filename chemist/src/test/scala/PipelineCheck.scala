ae6e024ef23e769162a4974af22fcf38638b9431
Paul Snively <paul.f.snively@one.verizon.com>
2015-10-29 11:45:21 -0700

    Fixed Sharders' construction of new Distribution, which makes 'newFlask works' ScalaCheck pass.

--- chemist/src/test/scala/PipelineCheck.scala
+++ chemist/src/test/scala/PipelineCheck.scala
@@ -11,13 +11,20 @@ import Sharding.Distribution
 
 import org.scalacheck.{ Arbitrary, Gen, Properties }
 import Arbitrary.arbitrary
-import org.scalacheck.Prop.forAll
+import Gen.{ alphaNumChar, listOf, listOfN, oneOf }
+import org.scalacheck.Prop.{ BooleanOperators, forAll }
 
 object PipelineSpecification extends Properties("Pipeline") {
+  /** Generates alphanumeric characters */
+  def alphaNumStr: Gen[String] =
+    listOfN(10, alphaNumChar).map(_.mkString).suchThat(_.forall(c => c.isDigit || c.isLetter))
+
   def genUUID = UUID.randomUUID()
   implicit lazy val arbUUID: Arbitrary[UUID] = Arbitrary(genUUID)
 
-  def genURI = new URI("http://localhost:8080/chemist/" + genUUID)
+  def genURI = for {
+    str <- alphaNumStr
+  } yield new URI("http://localhost:8080/chemist/" + str)
   implicit lazy val arbURI: Arbitrary[URI] = Arbitrary(genURI)
 
   def genTarget = for {
@@ -26,24 +33,24 @@ object PipelineSpecification extends Properties("Pipeline") {
   } yield Target(id.toString, uri)
   implicit lazy val arbTarget: Arbitrary[Target] = Arbitrary(genTarget)
 
-  implicit lazy val arbLocationIntent: Arbitrary[LocationIntent] = Arbitrary(Gen.oneOf(Mirroring, Ignored))
+  implicit lazy val arbLocationIntent: Arbitrary[LocationIntent] = Arbitrary(oneOf(Mirroring, Ignored))
 
   def genLocationTemplate = for {
-    template <- arbitrary[String]
+    template <- alphaNumStr
   } yield LocationTemplate(template)
   implicit lazy val arbLocationTemplate: Arbitrary[LocationTemplate] = Arbitrary(genLocationTemplate)
 
   def genLocation = for {
-    host <- arbitrary[String]
+    host <- alphaNumStr
     port <- arbitrary[Int]
-    datacenter <- arbitrary[String]
+    datacenter <- alphaNumStr
     intent <- arbitrary[LocationIntent]
     templates <- arbitrary[List[LocationTemplate]]
   } yield Location(host = host, port = port, datacenter = datacenter, intent = intent, templates = templates)
   implicit lazy val arbLocation: Arbitrary[Location] = Arbitrary(genLocation)
 
   def genFlaskID = for {
-    id <- arbitrary[String]
+    id <- alphaNumStr
   } yield FlaskID(id)
   implicit lazy val arbFlaskID: Arbitrary[FlaskID] = Arbitrary(genFlaskID)
 
@@ -57,4 +64,14 @@ object PipelineSpecification extends Properties("Pipeline") {
     pairs <- arbitrary[List[(Flask, Set[Target])]]
   } yield ==>>(pairs: _*)
   implicit lazy val arbDistribution: Arbitrary[Distribution] = Arbitrary(genDistribution)
+
+  property("newFlask works") = forAll { (f: Flask, d: Distribution) =>
+    val (nd, _) = Pipeline.handle.newFlask(f, RandomSharding)(d)
+    ("The existing Distribution does not contain the Flask" |:
+      !d.keySet.contains(f)) &&
+    ("The new Distribution contains the Flask" |:
+      nd.keySet.contains(f)) &&
+    ("The existing and new Distributions have the same Targets" |:
+      Sharding.targets(d) == Sharding.targets(nd))
+  }
 }
